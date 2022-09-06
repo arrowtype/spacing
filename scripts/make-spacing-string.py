eaed23d041c2e85c6dcc44b81115c482d7fb2915
arrowtype <arrowtypeco@gmail.com>
2022-09-06 11:07:00 -0400

    adjust spacing script to proof basic punctuation

--- scripts/make-spacing-string.py
+++ scripts/make-spacing-string.py
@@ -3,12 +3,13 @@
 """
 
 # Update to make different lists. copy these from your font editor as space-seperated glyph names
-glyphNames = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
+# characters = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
+characters = " . , : ; … ! ¡ ? ¿ · • * / \ - – — _ ( ) { } [ ] ‚ „ “ ” ‘ ’ \" ' ★ ♥ ✓ @ & © $ × ^ % ↑ → ↓ ←"
 
 # Configurable pattern. By default, this will put glyphs between nn and oo
 # Remove leading slashes and trailing spaces if you instead want to proof characters (not just glyph names), especially for InDesign, etc.
 # Add "\\n" at the end of your pattern if you want your output to include newlines, e.g. for the RoboFont space center
-pattern = "nn/$1 nono/$1 oo"
+pattern = "HH$1HOHO$1OO"
 
 # If you want to change the way each pattern is separated, change this. It adds a basic newline (`\n`) by default.
 # Change to a space (`" "`) if you want to only use spaces, e.g. as one option for an easy InDesign proof (newlines & columns might be better, though).
@@ -16,5 +17,5 @@ pattern = "nn/$1 nono/$1 oo"
 separator = "\n"
 
 # The code part. This goes through glyph name lists and outputs a proofing list.
-for name in glyphNames.split(" "):
+for name in characters.split(" "):
     print(pattern.replace("$1",name), end=separator)
