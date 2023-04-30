b66261ffc0cb43dcdce5f43a2f0a11b377e790cd
Stephen Nixon <arrowtypeco@gmail.com>
2023-04-29 22:26:04 -0400

    improve kerning string generatorf

--- scripts/make-kerning-string.py
+++ scripts/make-kerning-string.py
@@ -3,14 +3,15 @@
 """
 
 # space-separate list of glyph names for glyphs you want on the left side of pairs
-side1Names = "A F J K L T V W X Y"
+side1Names = "AFJKLTVWXY"
 
 # space-separate list of glyph names for glyphs you want on the right side of pairs
-side2Names = "A F J K T V W X Y"
+side2Names = "AFJKTVWXY"
 
 # Configure the kerning string pattern, if you want to adjust it. side1 glyphs will replace $1, and side2 glyphs will replace $2.
-# Remove leading slashes and trailing spaces if you instead want to proof characters (not just glyph names), especially for InDesign, etc.
-pattern = "HH/$1 /$2 HOHO/$1 /$2 OO nn/$1 /$2 nono/$1 /$2 oo"
+# Add/remove leading slashes and trailing spaces depending whether you want to proof characters or glyph names, especially for InDesign vs RoboFont, etc.
+pattern = "HH$1$2HO$1$2OO"
+# pattern = "nn$1$2no$1$2oo"
 
 # If you want to change the way each pattern is separated, change this. It adds a basic newline (`\n`) by default.
 # Change to a space (`" "`) if you want to only use spaces, e.g. as one option for an easy InDesign proof (newlines & columns might be better, though).
@@ -18,6 +19,6 @@ pattern = "HH/$1 /$2 HOHO/$1 /$2 OO nn/$1 /$2 nono/$1 /$2 oo"
 separator = "\n"
 
 # The code part. This goes through glyph name lists and outputs a proofing list.
-for glyphSide1 in side1Names.split(" "):
-    for glyphSide2 in side2Names.split(" "):
+for glyphSide1 in side1Names:
+    for glyphSide2 in side2Names:
         print(pattern.replace("$1",glyphSide1).replace("$2",glyphSide2), end=separator)
