0e1d1a5b4a9c327575e3b3040ea417438e69e881
arrowtype <arrowtypeco@gmail.com>
2025-03-10 10:51:35 -0400

    kerning permutation generators

--- scripts/make-kerning-permutations.py
+++ scripts/make-kerning-permutations.py
@@ -3,10 +3,16 @@
 """
 
 # space-separate list of glyph names for glyphs you want on the left side of pairs
-characters_1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-characters_2 = "abcdefghijklmnopqrstuvwxyz"
+# characters_1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# characters_2 = "abcdefghijklmnopqrstuvwxyz"
+characters_2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+characters_1 = ".,’‘“”'\"«»()[]{}_?¿!¡*:;"
+
+
 # characters = "0123456789"
 
 # The code part. This goes through glyph name lists and outputs a proofing list.
 for char_1 in characters_1:
-    print(char_1 + char_1.join([c for c in characters_2]) + char_1)
+    print("H", end="")
+    print(char_1 + char_1.join([c for c in characters_2]) + char_1, end="")
+    print("H")
---
+++ scripts/make-kerning-string-permutations-openclose.py
@@ -0,0 +1,36 @@
+"""
+    A simple way to make some kerning strings, for kerning and/or proofing your kerning.
+
+    Adjusted for open/close pairs, like brackets and quotes.
+"""
+
+
+# dict of open/close glyph pairs
+open_close_pairs = {
+    "‘": "’",
+    "“": "”",
+    "'": "'",
+    "\"": "\"",
+    "«": "»",
+    "‹": "›",
+    "{": "}",
+    "[": "]",
+    "(": ")",
+}
+
+# characters you want to check in the middle
+middleChars = "abcdefghijklmnopqrstuvwxyz"
+
+# Configure the kerning string pattern, if you want to adjust it.
+pattern = "nn{open}{middleChar}{close}nono{open}{middleChar}{close}oo"
+
+# If you want to change the way each pattern is separated, change this. It adds a basic newline (`\n`) by default.
+# Change to a space (`" "`) if you want to only use spaces, e.g. as one option for an easy InDesign proof (newlines & columns might be better, though).
+# Change to "\\n " at the end of your pattern if you want your output to include newlines in the RoboFont space center.
+separator = "\n"
+
+for open, close in open_close_pairs.items():
+    for middleChar in [c for c in middleChars]:
+        # use pattern as formatted string
+        print(pattern.format(open=open, close=close, middleChar=middleChar), end=separator)
+
